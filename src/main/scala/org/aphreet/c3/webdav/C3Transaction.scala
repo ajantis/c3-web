ac11b45507c24deaa96b503d44e07b0bb75aed6d
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-01-31 15:52:57 +0400

    [Add] Group-based access control for webdav [Add] webdav filesystem group directory is mapped to /<groupid>/files/ c3 fs directory

--- src/main/scala/org/aphreet/c3/webdav/C3Transaction.scala
+++ src/main/scala/org/aphreet/c3/webdav/C3Transaction.scala
@@ -3,8 +3,6 @@ package org.aphreet.c3.webdav
 import net.sf.webdav.ITransaction
 import collection.mutable
 import com.ifunsoftware.c3.access.fs.C3FileSystemNode
-import collection.mutable.ArrayBuffer
-import com.ifunsoftware.c3.access.C3ByteChannel
 import java.security.Principal
 import org.aphreet.c3.model.{Group, User}
 
@@ -14,10 +12,8 @@ class C3Transaction(val principal:Principal) extends ITransaction{
 
   val cachedFiles = new mutable.HashMap[String, C3FileSystemNode]
 
-  val openedChannels = new mutable.HashSet[C3ByteChannel]
-
   def close() {
-    openedChannels.foreach(_.close())
+
   }
 }
 
