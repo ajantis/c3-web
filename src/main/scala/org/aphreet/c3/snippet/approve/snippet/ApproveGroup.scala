2669d6a7c6d128c04fa2e89d017d39d7901f4209
Dmitry Ivanov <id.ajantis@gmail.com>
2013-09-01 14:57:48 +0200

    Code factoring for ApproveGroup snippet: making a little bit more readable =)

--- src/main/scala/org/aphreet/c3/snippet/approve/snippet/ApproveGroup.scala
+++ src/main/scala/org/aphreet/c3/snippet/approve/snippet/ApproveGroup.scala
@@ -20,53 +20,65 @@ import org.aphreet.c3.service.notifications.{ApproveGroupMsg, AddedToGroupMsg}
  */
 class ApproveGroup {
 
-  lazy val groupService = DependencyFactory.inject[GroupService].open_!
-  lazy val notificationManager = inject[NotificationManagerRef].open_!.actorRef
+  lazy val groupService = DependencyFactory.inject[GroupService]
+    .openOrThrowException("Group service should be always here")
+  lazy val notificationManager = inject[NotificationManagerRef]
+    .openOrThrowException("Notification manager should be here").actorRef
 
   def render = {
+    val groupsToApprove = Group.findAll(By(Group.isApprove, false))
 
-    val listApproveGroup = Group.findAll().filter(_.isApprove!=true)
+    ".list_group_approve" #> groupsToApprove.map {
+      group: Group => {
+        val owner = User.find(group.owner).openOrThrowException(s"No owner for ${group.name}")
 
-    ".list_group_approve"#> listApproveGroup.map(group=>{
+        def approveGroup(): JsCmd = {
+          group.getGroupC3 match {
+            case Full(groupFileNode)             =>
+              // we don't need to create a C3 mapping for this group, it already exists
+              processGroupApproval(group, owner) &
+              LiftMessages.ajaxNotice(s"Group ${group.name} is approved")
 
-      val owner = User.find(group.owner).openOrThrowException("None Owner")
+            case Failure("File not found", _, _) =>
+              // we need to create a C3 mapping first and then create a group
+              createAndApprove(group, owner)
 
-      def approveGroup():JsCmd = {
 
-        group.getGroupC3 match {
-          case Full(groupC3) =>{
-            group.isApprove(true).save
-            notificationManager ! CreateNotification(ApproveGroupMsg(group,owner.id.is))
-            JsCmds.Replace(group.id.is.toString, NodeSeq.Empty)&
-            LiftMessages.ajaxNotice("Exists group "+ group.name +" is approve")
+            case Failure(msg, _, _)              =>
+              LiftMessages.ajaxError(msg)
           }
-          case Failure("File not found",_, _) => groupService.createGroup(group, group.tags, group.description) match {
-            case Full(g) =>
-              group.isApprove(true).save
-              notificationManager ! CreateNotification(ApproveGroupMsg(group,owner.id.is))
-              JsCmds.Replace(group.id.is.toString, NodeSeq.Empty)&
-              LiftMessages.ajaxNotice(g.name +" is create and approve")
-
-            case Failure(msg, _, _) => LiftMessages.ajaxError(msg)
-            case _ => LiftMessages.ajaxError("Group is not approve")
-          }
-          case Failure(msg, _, _) => LiftMessages.ajaxError(msg)
         }
 
+        val groupTags = group.getTags
+
+        ".tags_group"              #> groupTags.map { ".tags_group *" #> _ } &
+        ".list_group_approve [id]" #> group.id &
+        ".group_name *"            #> group.name &
+        ".group_description *"     #> group.description &
+        ".group_owner *"           #> owner.niceName&
+        ".approve_group [onclick]" #> SHtml.ajaxInvoke(()=>approveGroup())
+
       }
-
-      val groupTags = group.getTags
-      ".tags_group" #> groupTags.map((tag: String) => {
-        ".tags_group *" #> tag
-      }) &
-      ".list_group_approve [id]"#> group.id &
-      ".group_name *" #> group.name &
-      ".group_description *" #> group.description &
-      ".group_owner *" #> owner.niceName&
-      ".approve_group [onclick]"#> SHtml.ajaxInvoke(()=>approveGroup())
-
     }
+  }
 
-    )
+  private def processGroupApproval(group: Group, owner: User): JsCmd = {
+    group.isApprove(true).save
+    notificationManager ! CreateNotification(ApproveGroupMsg(group, owner.id.is))
+
+    JsCmds.Replace(group.id.is.toString, NodeSeq.Empty)
+  }
+
+  private def createAndApprove(group: Group, owner: User): JsCmd = {
+    groupService.createGroup(group, group.tags, group.description) match {
+      case Full(g) =>
+        processGroupApproval(group, owner) & LiftMessages.ajaxNotice(s"Group ${g.name} is approved and created")
+
+      case Failure(msg, _, _) =>
+        LiftMessages.ajaxError(msg)
+
+      case _                  =>
+        LiftMessages.ajaxError("Group is not approved")
+    }
   }
 }
