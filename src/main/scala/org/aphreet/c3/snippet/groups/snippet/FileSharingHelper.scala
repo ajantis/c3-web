f52374f2ee26785c570f508392f6d88e8d9dc768
mrproz <3591957par>
2014-06-08 15:17:40 +0400

    [Edit] self-documenting code coming. [Merge] with main repo

--- src/main/scala/org/aphreet/c3/snippet/groups/snippet/FileSharingHelper.scala
+++ src/main/scala/org/aphreet/c3/snippet/groups/snippet/FileSharingHelper.scala
@@ -12,9 +12,17 @@ import net.liftweb.util._
 
 /**
  * @author a-legotin
+ * @define shall provide a resource link
+ * @example FileSharingHelper.shareFile(node)
+ * @version 1.0
  */
 object FileSharingHelper {
 
+  /**
+   * @define enable sharing for file, provide access link
+   * @param node C3FileSystemNode (C3 file or directory)
+   * @return JavaScript
+   */
   def shareFile(node: C3FileSystemNode): JsCmd = {
     try {
 
@@ -22,12 +30,16 @@ object FileSharingHelper {
       val hash = md5Hash(node.name + timestamp)
       node.update(MetadataUpdate((Map(HASH -> C3System.stringToMetadataValue(hash)))))
       JsCmds.SetValById("txtHash",fileShareFullUrl(node.asFile))
-
     } catch {
       case e: Exception => JsCmds.Alert("Failed to sharing file")
     }
   }
 
+  /**
+   * @define disable sharing for file, delete access link
+   * @param node C3FileSystemNode (C3 file or directory)
+   * @return JavaScript
+   */
   def disableSharing(node: C3FileSystemNode): JsCmd = {
     try {
       node.update(MetadataUpdate((Map(HASH -> C3System.stringToMetadataValue("")))))
@@ -39,21 +51,30 @@ object FileSharingHelper {
   }
 
   /**
-   * @author a-legotin
    * @define Generate MD5 hash for link building
-   * @usecase md5Hash("Name")
+   * @example md5Hash("Name")
    * @param text wich need to generate md5 hash
    * @return genereted md5hash
    */
   def md5Hash(text: String) : String = java.security.MessageDigest.getInstance("MD5").digest(text.getBytes()).map(0xFF & _).map { "%02x".format(_) }.foldLeft(""){_ + _}
 
+  /**
+   * @define Provide shared relative path to C3 file
+   * @param file C3File
+   * @param hash MD5 hash
+   * @return shared relative path
+   */
   def fileSharePath(file: C3File, hash: String): String = {
     var splittedFullPath = file.fullname.split("/")
     val name = Helpers.urlEncode(splittedFullPath.last)
     val path = splittedFullPath.dropRight(splittedFullPath.indexOf(splittedFullPath.last) - 2 ).mkString("/")
     s"/sharing$path/$name?id=$hash"
   }
-
+  /**
+   * @define Provide shared absolute path to C3 file
+   * @param file C3File
+   * @return shared absolute path
+   */
   def fileShareFullUrl(file: C3File): String = {
     val scheme = S.request.map(_.request.scheme).openOr("")
     val server = S.request.map(_.request.serverName).openOr("")
