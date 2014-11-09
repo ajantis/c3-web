892f47225a757b7b56e63d35549207795491079e
nicl-nno <nicl.nno@gmail.com>
2014-11-09 22:30:26 +0400

    Transfer file between directory within group implemented (issue #16)

---
+++ src/main/scala/org/aphreet/c3/snippet/groups/snippet/FileTransferHelper.scala
@@ -0,0 +1,27 @@
+package org.aphreet.c3.snippet.groups.snippet
+import org.aphreet.c3.model.{ User, Group }
+
+/**
+ * @author nicl.nno
+ * @define shall move file to another directory within group
+ * @version 1.0
+ */
+object FileTransferHelper {
+  var draggableResourceName = "";
+
+  def moveSelectedFile(group: Group, currentAddress: String, targetFileFolder: String, moveBack: Boolean) = {
+    if (draggableResourceName != "") {
+      val movableFile = group.getFile(currentAddress + draggableResourceName)
+      movableFile.foreach {
+        f =>
+          var newPath = (f.fullname.split("/").toList.init ::: targetFileFolder + "/" + draggableResourceName :: Nil).mkString("", "/", "")
+          if (moveBack) newPath = targetFileFolder.substring(targetFileFolder.drop(1).indexOf('/') + 1, targetFileFolder.length) + "/" + draggableResourceName
+          f.move(newPath)
+      }
+    }
+  }
+
+  def saveDraggableResourceName(resourceName: String) = {
+    draggableResourceName = resourceName
+  }
+}
