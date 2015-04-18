efc4ea63dad36b8a65544282dbcca5ce85ee94ab
Valentin Korolev <kor.val@inbox.ru>
2015-04-18 13:43:32 +0300

    Groups page tabs and header for unauthorized users - fixed

--- src/main/scala/org/aphreet/c3/snippet/groups/snippet/GroupListPage.scala
+++ src/main/scala/org/aphreet/c3/snippet/groups/snippet/GroupListPage.scala
@@ -1,7 +1,7 @@
 package org.aphreet.c3.snippet.groups.snippet
 
 import com.ifunsoftware.c3.access.C3System
-import net.liftweb.common.{Empty, Logger}
+import net.liftweb.common.{Box, Empty, Logger, Full}
 import org.aphreet.c3.lib.DependencyFactory._
 import xml.NodeSeq
 import org.aphreet.c3.model._
@@ -12,7 +12,6 @@ import net.liftweb.util.Helpers._
 import org.aphreet.c3.lib.DependencyFactory
 import net.liftweb.util.CssSel
 import net.liftweb.mapper.By
-import net.liftweb.common.Full
 import org.aphreet.c3.acl.groups.{UserStatusGroup, GroupsAccess}
 import org.aphreet.c3.snippet.LiftMessages
 import com.ibm.icu.text.Transliterator
@@ -22,7 +21,7 @@ import java.lang.String;
 /**
  * @author Koyushev Sergey (mailto: serjk91@gmail.com)
  */
-class GroupListPage extends GroupsAccess{
+class GroupListPage extends GroupsAccess {
 
   lazy val c3 = DependencyFactory.inject[C3System].open_!
 
@@ -38,16 +37,17 @@ class GroupListPage extends GroupsAccess{
   val MyOwnGroupsParameter: String = "myOwnGroups"
   val MyGroupsParameter: String = "myGroups"
   val activeClass = "active"
+
   def userListGroup = {
     val groupList = User.currentUser match {
       case Full(u) => u.groups.toList
     }
     ".container_groupList" #> groupList.distinct.sortBy(_.name.is).filter(_.isApproved).map {
       group: Group =>
-        def infoGroup():CssSel = {
+        def infoGroup(): CssSel = {
 
           val groupLink = s"/groups/${group.getId}/files/"
-            ".name_group *" #> group.name.is &
+          ".name_group *" #> group.name.is &
             "a [href]" #> groupLink &
             ".event *" #> "3"
         }
@@ -55,6 +55,7 @@ class GroupListPage extends GroupsAccess{
         infoGroup()
     }
   }
+
   def list = {
 
     val tab = S.param("tab") openOr MyOwnGroupsParameter
@@ -69,85 +70,94 @@ class GroupListPage extends GroupsAccess{
       case Empty => Group.findAll(By(Group.isOpen, true))
     }
 
-      "#header *" #> (tab match {
-        case MyGroupsParameter => "My Groups"
-        case MyOwnGroupsParameter => "My Own Groups"
-        case _ => "All Groups"
-      }) &
-      (User.currentUser match {
-        case Full(u)=> "#add_group" #> addGroup()
-        case Empty =>
-          ".btn_add_user" #> NodeSeq.Empty &
-            "#add_group" #> NodeSeq.Empty
-      }) &
+    pageHeader(User.currentUser, tab) &
+    (User.currentUser match {
+      case Full(u) => "#add_group" #> addGroup()
+      case Empty =>
+        ".btn_add_user" #> NodeSeq.Empty &
+          "#add_group" #> NodeSeq.Empty
+    }) &
       ".container_groups" #> groupList.distinct.sortBy(_.name.is).filter(_.isApproved).map {
         group: Group =>
 
-      val groupIcon = if(group.isOpen.is) openGroupIcon else lockGroupIcon
+          val groupIcon = if (group.isOpen.is) openGroupIcon else lockGroupIcon
 
-      def infoGroup(picName:String):CssSel = {
+          def infoGroup(picName: String): CssSel = {
 
-        val groupLink = s"/groups/${group.getId}/files/"
-        val groupTags = group.getTags
-        ".tags_group" #> groupTags.map((tag: String) => {
-          ".label *" #> tag
-        }) &
-          ".inf_left_groups [src]"#> ("/images/"+picName)&
-          "a *" #> group.name.is &
-          "a [href]" #> groupLink &
-          ".description_group *"#> group.getDescription &
-          ".owner_group  *" #> group.owner.obj.map(_.shortName).openOr("N/A")
+            val groupLink = s"/groups/${group.getId}/files/"
+            val groupTags = group.getTags
+            ".tags_group" #> groupTags.map((tag: String) => {
+              ".label *" #> tag
+            }) &
+              ".inf_left_groups [src]" #> ("/images/" + picName) &
+              "a *" #> group.name.is &
+              "a [href]" #> groupLink &
+              ".description_group *" #> group.getDescription &
+              ".owner_group  *" #> group.owner.obj.map(_.shortName).openOr("N/A")
+          }
+          def adminGroup(): CssSel = {
+            val settingsLink = s"/groups/${group.getId}/settings"
+            userGroup() &
+              ".cog [onClick]" #> SHtml.ajaxInvoke(() => JsCmds.RedirectTo(settingsLink))
+          }
+
+          def userGroup(): CssSel = {
+            ".plus" #> NodeSeq.Empty &
+              ".event *" #> "3" //[TODO] add count events & redirect to event page
+          }
+
+          def lockGroup(): CssSel = {
+            ".cog" #> NodeSeq.Empty &
+              ".event" #> NodeSeq.Empty
+          }
+
+          def sendRequest(): JsCmd = {
+            val user = User.currentUserUnsafe
+            groupService.addUsersToApproveListGroup(group, Iterable(user))
+            LiftMessages.ajaxNotice(user.niceName + " is add to approve list of group " + group.name.is) &
+              JsCmds.Replace(group.getId, NodeSeq.Empty)
+          }
+
+          (User.currentUser match {
+            case Full(u) => checkAccess(u, group) match {
+              case UserStatusGroup.Admin =>
+                adminGroup()
+
+              case UserStatusGroup.Owner =>
+                adminGroup()
+
+              case UserStatusGroup.Member =>
+                userGroup() &
+                  ".cog" #> NodeSeq.Empty
+
+              case UserStatusGroup.Request =>
+                lockGroup() &
+                  ".plus" #> NodeSeq.Empty
+
+              case UserStatusGroup.Other =>
+                lockGroup() &
+                  ".plus [id]" #> group.getId &
+                  ".plus [onclick]" #> SHtml.ajaxInvoke(() => sendRequest())
+            }
+            case Empty =>
+              lockGroup() &
+                ".plus [onclick]" #> SHtml.ajaxInvoke(() => LiftMessages.ajaxNotice(S.?("response.login")))
+          }) &
+            infoGroup(groupIcon)
       }
-      def adminGroup():CssSel = {
-        val settingsLink = s"/groups/${group.getId}/settings"
-        userGroup()&
-          ".cog [onClick]" #> SHtml.ajaxInvoke(()=> JsCmds.RedirectTo(settingsLink))
-      }
+  }
 
-      def userGroup():CssSel = {
-        ".plus" #> NodeSeq.Empty &
-          ".event *" #> "3" //[TODO] add count events & redirect to event page
-      }
-
-      def lockGroup():CssSel = {
-        ".cog" #> NodeSeq.Empty&
-          ".event" #> NodeSeq.Empty
-      }
-
-      def sendRequest():JsCmd = {
-        val user = User.currentUserUnsafe
-        groupService.addUsersToApproveListGroup(group,Iterable(user))
-        LiftMessages.ajaxNotice(user.niceName +" is add to approve list of group " + group.name.is)&
-          JsCmds.Replace(group.getId, NodeSeq.Empty)
-      }
-
-      (User.currentUser match {
-        case Full(u) => checkAccess(u,group) match {
-          case UserStatusGroup.Admin =>
-            adminGroup()
-
-          case UserStatusGroup.Owner   =>
-            adminGroup()
-
-          case UserStatusGroup.Member  =>
-            userGroup()&
-              ".cog" #> NodeSeq.Empty
-
-          case UserStatusGroup.Request =>
-            lockGroup()&
-              ".plus" #> NodeSeq.Empty
-
-          case UserStatusGroup.Other   =>
-            lockGroup()&
-              ".plus [id]" #> group.getId &
-              ".plus [onclick]" #> SHtml.ajaxInvoke(()=> sendRequest())
-        }
-        case Empty =>
-          lockGroup()&
-            ".plus [onclick]" #> SHtml.ajaxInvoke(()=>  LiftMessages.ajaxNotice(S.?("response.login")))
-      })&
-        infoGroup(groupIcon)
-    }
+  private def pageHeader(currentUser: Box[User], tab: String) = {
+    "#header *" #> (
+      currentUser match {
+        case Full(u) => (
+          tab match {
+            case MyGroupsParameter => "My Groups"
+            case MyOwnGroupsParameter => "My Own Groups"
+            case _ => "All Groups"
+          })
+        case Empty => "All Groups"
+      })
   }
 
   def newGroup = User.currentUser match {
@@ -159,18 +169,25 @@ class GroupListPage extends GroupsAccess{
 
   def tabs = {
     val tab = S.param("tab") openOr MyGroupsParameter
-    tab match {
-      case MyGroupsParameter => "#myGroups [class+]" #> activeClass
-      case MyOwnGroupsParameter => "#myOwnGroups [class+]" #> activeClass
-      case _ => "#all [class+]" #> activeClass
+    User.currentUser match {
+      case Full(u) => "#add_group" #> addGroup() &
+        (tab match {
+          case MyGroupsParameter => "#myGroups [class+]" #> activeClass
+          case MyOwnGroupsParameter => "#myOwnGroups [class+]" #> activeClass
+          case _ => "#all [class+]" #> activeClass
+        })
+      case Empty =>
+        "#myGroups" #> NodeSeq.Empty &
+          "#myOwnGroups" #> NodeSeq.Empty &
+          "#all" #> NodeSeq.Empty
     }
   }
 
-  def addGroup():CssSel = {
+  def addGroup(): CssSel = {
     var newGroup = Group.create
     var public = ""
 
-    def saveMe(){
+    def saveMe() {
       newGroup.validate match {
         case Nil =>
           val groupUID = GetGroupUID(newGroup.name)
@@ -178,32 +195,32 @@ class GroupListPage extends GroupsAccess{
           newGroup = newGroup.owner(User.currentUserUnsafe)
 
           if (public != "false") newGroup.isOpen(true)
-          if(newGroup.save) S.notice(S.?("approve.list.group") + newGroup.name)
+          if (newGroup.save) S.notice(S.?("approve.list.group") + newGroup.name)
           else S.warning(newGroup.name + " isn't added")
 
         case xs =>
           xs.foreach(f => S.error(f.msg))
       }
     }
-    "name=name" #> SHtml.onSubmit(newGroup.name(_))&
-      "name=description" #> SHtml.onSubmit(newGroup.description(_))&
+    "name=name" #> SHtml.onSubmit(newGroup.name(_)) &
+      "name=description" #> SHtml.onSubmit(newGroup.description(_)) &
       "name=public" #> SHtml.onSubmit(public = _) &
       "name=tags_edit" #> SHtml.onSubmit(newGroup.tags(_)) &
       "type=submit" #> SHtml.onSubmitUnit(saveMe)
   }
 
-  def GetGroupUID(groupName:String): String = {
+  def GetGroupUID(groupName: String): String = {
     val cyrillicToLatin = Transliterator.getInstance("Any-Latin; NFD; [^\\p{Alnum}] Remove")
     val latinGroupName = cyrillicToLatin.transliterate(groupName).replace(" ", "_")
 
-    var uniqueGroupName = latinGroupName.replaceAll("[^A-Za-z0-9_-]","")
+    var uniqueGroupName = latinGroupName.replaceAll("[^A-Za-z0-9_-]", "")
 
     var additionalId = 1;
     val c3 = inject[C3System].open_!
 
-    while (c3.getFile("/").asDirectory.children().exists(c=>c.name == uniqueGroupName)) {
-      additionalId=additionalId + 1
-      uniqueGroupName = uniqueGroupName+"_"+additionalId.toString
+    while (c3.getFile("/").asDirectory.children().exists(c => c.name == uniqueGroupName)) {
+      additionalId = additionalId + 1
+      uniqueGroupName = uniqueGroupName + "_" + additionalId.toString
     }
     return uniqueGroupName
   }
